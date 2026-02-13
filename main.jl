11e315e9b6e9ef88d60792626cfee39c35c047d0
alanflores-cmd <alanclakflores@gmail.com>
2026-02-12 22:11:27 -0600

    Implement personalized greeting function
    Added a personalized greeting function and test.

--- main.jl
+++ main.jl
@@ -1,1 +1,9 @@
 println("Hola Mundo")
+
+# Función de saludo personalizada
+function saludar(nombre)
+    return "Hola, $nombre, bienvenido a Ciencia de Datos."
+end
+
+# Prueba de la función
+println(saludar("Estudiante B"))
